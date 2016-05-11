c3a5303d138278239c4cda768a00e0de2c4c198d
ityoung <ityoung@126.com>
2016-05-11 11:29:25 +0800

    create dir

--- mhome_test/screen_shot.py
+++ mhome_test/screen_shot.py
@@ -1,5 +1,6 @@
 from appium import webdriver
 import time
+import os
 import sys
 import logging
 
@@ -7,6 +8,9 @@ TIME_format = "%y%m%d%H%M%S"
 img_path_base = 'F:/mhome/mhome_test/screeshot/'
 
 def screenshot(driver):
+    isExist = os.path.exists(img_path_base)
+    if not isExist:
+        os.makedirs(img_path_base)
     timestamp = time.strftime(TIME_format,time.localtime())
     screenshot = driver.get_screenshot_as_file(img_path_base+\
         sys._getframe().f_back.f_code.co_name+timestamp+'.png')
